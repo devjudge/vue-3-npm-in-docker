5d83bdf896708dc171322d69886c598d53d1220a
Rajat Tyagi <40917672+tyagirajat200@users.noreply.github.com>
2021-09-07 18:40:07 +0530

    Update chrome-test-setup.sh

--- chrome-test-setup.sh
+++ chrome-test-setup.sh
@@ -3,12 +3,12 @@ sudo apt-get update
 
 echo "Installing Chrome for Selenium.."
 cd /tmp
-wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_80.0.3987.116-1_amd64.deb
-sudo apt install --assume-yes ./google-chrome-stable_80.0.3987.116-1_amd64.deb
+wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_91.0.4472.164-1_amd64.deb
+sudo apt install --assume-yes ./google-chrome-stable_91.0.4472.164-1_amd64.deb
 
 echo "Installing chromedriver for Selenium..."
 cd /tmp
-curl https://chromedriver.storage.googleapis.com/80.0.3987.106/chromedriver_linux64.zip > /tmp/chromedriver.zip
+curl https://chromedriver.storage.googleapis.com/91.0.4472.101/chromedriver_linux64.zip > /tmp/chromedriver.zip
 unzip chromedriver.zip
 sudo mv chromedriver /usr/bin/chromedriver
 chromedriver --version
