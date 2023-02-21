5aeff8847be326ede3b5fd3552b6a545f6fc10af
Rajat Tyagi <40917672+tyagirajat200@users.noreply.github.com>
2023-02-21 13:25:54 +0530

    Update chrome-test-setup.sh

--- chrome-test-setup.sh
+++ chrome-test-setup.sh
@@ -3,14 +3,14 @@ sudo apt-get update
 
 echo "Installing Chrome for Selenium.."
 cd /tmp
-wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_91.0.4472.164-1_amd64.deb
-sudo apt install --assume-yes ./google-chrome-stable_91.0.4472.164-1_amd64.deb
+wget https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_110.0.5481.77-1_amd64.deb
+sudo apt install --assume-yes ./google-chrome-stable_110.0.5481.77-1_amd64.deb
 
 echo "Installing chromedriver for Selenium..."
 cd /tmp
-curl https://chromedriver.storage.googleapis.com/91.0.4472.101/chromedriver_linux64.zip > /tmp/chromedriver.zip
+curl https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip > /tmp/chromedriver.zip
 unzip chromedriver.zip
 sudo mv chromedriver /usr/bin/chromedriver
 chromedriver --version
 
-chromedriver --whitelisted-ips
+chromedriver --whitelisted-ips --allowed-origins='*'
